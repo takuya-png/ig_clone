bf6fd562c4ab4c4ab826976fda14a1b554a7674f
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-07 14:46:39 +0900

    new,create,update,show,editアクション追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,42 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: "ユーザが作成されました！"
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "投稿できました！"
+    else
+      render :edit
+    end
   end
 
   def show
+    @users = User.find(params[:id])
+    redirect_to picture_blogs_path, notice: "不正操作を記録しました。" unless current_user.id == @users.id
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+  end
+
 end
