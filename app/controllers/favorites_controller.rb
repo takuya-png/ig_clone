4b85277a11af97062de384d3c5f592d99ebf0fe1
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-07 18:03:07 +0900

    お気に入り機能作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,15 @@
 class FavoritesController < ApplicationController
   def index
+    @favorites = Favorite.all
+  end
+  
+  def create
+    favorite = current_user.favorites.create(picture_blog_id: params[:picture_blog_id])
+    redirect_to picture_blogs_path, notice: "#{favorite.picture_blog.user.name}さんのブログをお気に入り登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to picture_blogs_path, notice: "#{favorite.picture_blog.user.name}さんのブログをお気に入り解除しました"
   end
 end
